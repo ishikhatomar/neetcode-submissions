20f0571baace7f1d868515d62f9b0220e94baab0
Shikha Tomar <147544277+ishikhatomar@users.noreply.github.com>
2026-09-21 13:37:28 +0530

    Contains Duplicate Element
    Implement function to check for duplicates in an array.

---
+++ NeetCode 75/Arrays & Hashing/Contains Duplicate.cpp
@@ -0,0 +1,13 @@
+//Given an integer array nums, return true if any value appears more than once in the array; otherwise return false.
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+      unordered_set<int> st;
+      for(int i=0; i<nums.size(); i++){
+            if(st.count(nums[i]))
+                return true;
+            st.insert(nums[i]);
+      }
+      return false;  
+    }
+};
