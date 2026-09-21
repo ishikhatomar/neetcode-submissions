5018f46d54d5957b1c0b11bb14c7be8efd82eaaf
Shikha Tomar <147544277+ishikhatomar@users.noreply.github.com>
2026-09-21 14:10:43 +0530

    Add: three-integer-sum - submission-0

---
+++ Data Structures & Algorithms/three-integer-sum/submission-0.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+        vector<vector<int>> ans;
+        for(int i =0; i<n; i++){
+            if(i >0 && nums[i-1]==nums[i]) continue;
+            int j = i+1, k=n-1;
+            
+            while(j<k){
+                int sum = nums[i] + nums[j]+nums[k];
+                if(sum == 0){
+                    ans.push_back({nums[i],nums[j],nums[k]});
+                    j++;
+                    k--;
+                    while(j<k && nums[j-1]==nums[j])j++;
+                    while(j<k && nums[k]==nums[k+1])k--;
+                }
+                else if(sum <0) j++;
+                else k--;
+            }
+        }
+        return ans;
+    }
+};
